a203fa748dce0f7884d52264cb553dd4de0ce3c3
Efrasius <33842173+Efrasius@users.noreply.github.com>
2022-07-19 18:03:07 +0200

    imageEditor
    Ajout du module imageEditor et paramétrage

---
+++ character-sheet/src/components/customImageEditor.jsx
@@ -0,0 +1,48 @@
+import 'tui-image-editor/dist/tui-image-editor.css';
+import ImageEditor from '@toast-ui/react-image-editor';
+
+const myTheme = {
+  // Theme object to extends default dark theme.
+};
+
+const locale_fr_FR = {
+    Shape: 'Ajouter des formes',
+    Filter: 'Filtres',
+    Grayscale: 'Noir et blanc',
+    Blur: 'Flou',
+    Emboss: 'Bosseler',
+    Mask: 'Ajouter une image',
+    Text: 'Ajouter du texte',
+    Crop: 'Découper',
+    Download: 'Générer fiche',
+    Load: 'Changer fond'
+}
+
+const SheetEditor = () => (
+  <ImageEditor
+    includeUI={{
+      loadImage: {
+        path: './images/bann.jpg',
+        name: 'SampleImage',
+      },
+      locale: locale_fr_FR,
+      theme: myTheme,
+      menu: ['shape', 'filter', 'mask', 'text', 'crop', 'icon'],
+      initMenu: 'filter',
+      uiSize: {
+        width: '1000px',
+        height: '700px',
+      },
+      menuBarPosition: 'top',
+    }}
+    cssMaxHeight={500}
+    cssMaxWidth={700}
+    selectionStyle={{
+      cornerSize: 20,
+      rotatingPointOffset: 70,
+    }}
+    usageStatistics={true}
+  />
+);
+
+export default SheetEditor;
--- character-sheet/src/components/sheetForm.jsx
+++ character-sheet/src/components/sheetForm.jsx
@@ -1,6 +1,12 @@
 import React, { useState } from 'react';
 import '../style/sheetForm.css'
-import {Dropzone, FileItem} from "@dropzone-ui/react"
+//import {Dropzone, FileItem} from "@dropzone-ui/react"
+import SheetEditor from './customImageEditor';
+
+
+const myTheme = {
+  // Theme object to extends default dark theme.
+};
 
 function SheetForm() {
     const [lodestoneId, setLodestoneId] = useState("");
@@ -8,15 +14,15 @@ function SheetForm() {
     const [err, setErr] = useState("");
 
     //dropzoneUI setup
-    const [files, setFiles] = React.useState([]);
-    const updateFiles = (incommingFiles) => {
+    //const [files, setFiles] = React.useState([]);
+    /*const updateFiles = (incommingFiles) => {
     setFiles(incommingFiles);
-  };
+  };*/
 
     const launchApi = async (event) => {
         event.preventDefault();
 
-        if ({lodestoneId} === "") {
+        if (lodestoneId.length === 0) {
             alert("Veuillez entrer l'id lodestone de votre personnage")
         } else {
             try {
@@ -32,57 +38,54 @@ function SheetForm() {
     }
 
     return (
-        <form className="myForm" onSubmit={launchApi}>
-            <div className="formLabeled">
-                <label className="formLabel" htmlFor="lodestoneId">Lodestone ID</label>
-                <input id="lodestoneId" className="formInput" name="lodestoneId" value={lodestoneId} onChange={(e) => setLodestoneId(e.target.value)} type="text" />
+        <div>
+            <form className="myForm" onSubmit={launchApi}>
+                <div className="formLabeled">
+                    <label className="formLabel" htmlFor="lodestoneId">Lodestone ID</label>
+                    <input id="lodestoneId" className="formInput" name="lodestoneId" value={lodestoneId} onChange={(e) => setLodestoneId(e.target.value)} type="text" />
+                </div>
+                <div className="formLabeled">
+                    <label className="formLabel" htmlFor="mainJob">Main Job</label>
+                    <select id="mainJob" name="mainJob" className="formInput" value={mainJob} onChange={(e) => setmainJob(e.target.value)}>
+                        <option value="Paladin">Paladin</option>
+                        <option value="Guerrier">Guerrier</option>
+                        <option value="chevalier-noir">Chevalier Noir</option>
+                        <option value="pistosabreur">Pistosabreur</option>
+                        <option value="mage-blanc">Mage Blanc</option>
+                        <option value="erudit">Erudit</option>
+                        <option value="astromancien">Astromancien</option>
+                        <option value="sage">Sage</option>
+                        <option value="moine">Moine</option>
+                        <option value="chevalier-dragon">Chevalier Dragon</option>
+                        <option value="ninja">Ninja</option>
+                        <option value="samourai">Samouraï</option>
+                        <option value="faucheur">Faucheur</option>
+                        <option value="barde">Barde</option>
+                        <option value="machiniste">Machiniste</option>
+                        <option value="danseur">Danseur</option>
+                        <option value="mage-noir">Mage Noir</option>
+                        <option value="invocateur">Invocateur</option>
+                        <option value="mage-rouge">Mage Rouge</option>
+                        <option value="mage-bleu">Mage Bleu</option>
+                        <option value="menuisier">Menuisier</option>
+                        <option value="forgeron">Forgeron</option>
+                        <option value="armurier">Armurier</option>
+                        <option value="orfevre">Orfèvre</option>
+                        <option value="tanneur">Tanneur</option>
+                        <option value="couturier">Couturier</option>
+                        <option value="alchimiste">Alchimiste</option>
+                        <option value="cuisinier">Cuisinier</option>
+                        <option value="mineur">Mineur</option>
+                        <option value="botaniste">Botaniste</option>
+                        <option value="pecheur">Pêcheur</option>
+                    </select>
+                </div>
+                <input type="submit" className="formSubmit" value="Générer Ma fiche !" onClick={launchApi} />
+            </form>
+            <div className="editor">
+                <SheetEditor />
             </div>
-            <div className="formLabeled">
-                <label className="formLabel" htmlFor="mainJob">Main Job</label>
-                <select id="mainJob" name="mainJob" className="formInput" value={mainJob} onChange={(e) => setmainJob(e.target.value)}>
-                    <option value="Paladin">Paladin</option>
-                    <option value="Guerrier">Guerrier</option>
-                    <option value="chevalier-noir">Chevalier Noir</option>
-                    <option value="pistosabreur">Pistosabreur</option>
-                    <option value="mage-blanc">Mage Blanc</option>
-                    <option value="erudit">Erudit</option>
-                    <option value="astromancien">Astromancien</option>
-                    <option value="sage">Sage</option>
-                    <option value="moine">Moine</option>
-                    <option value="chevalier-dragon">Chevalier Dragon</option>
-                    <option value="ninja">Ninja</option>
-                    <option value="samourai">Samouraï</option>
-                    <option value="faucheur">Faucheur</option>
-                    <option value="barde">Barde</option>
-                    <option value="machiniste">Machiniste</option>
-                    <option value="danseur">Danseur</option>
-                    <option value="mage-noir">Mage Noir</option>
-                    <option value="invocateur">Invocateur</option>
-                    <option value="mage-rouge">Mage Rouge</option>
-                    <option value="mage-bleu">Mage Bleu</option>
-                    <option value="menuisier">Menuisier</option>
-                    <option value="forgeron">Forgeron</option>
-                    <option value="armurier">Armurier</option>
-                    <option value="orfevre">Orfèvre</option>
-                    <option value="tanneur">Tanneur</option>
-                    <option value="couturier">Couturier</option>
-                    <option value="alchimiste">Alchimiste</option>
-                    <option value="cuisinier">Cuisinier</option>
-                    <option value="mineur">Mineur</option>
-                    <option value="botaniste">Botaniste</option>
-                    <option value="pecheur">Pêcheur</option>
-                </select>
-            </div>
-            <div className="formLabeled">
-                <label className="formLabel" htmlFor="formImage">Image du personnage</label>
-                <Dropzone accept="image/*" localization="FR-fr" maxFiles="1" onChange={updateFiles} value={files}>
-                    {files.map((file) => (
-                        <FileItem {...file} preview />
-                    ))}
-                </Dropzone>
-            </div>
-            <input type="submit" className="formSubmit" value="Générer Ma fiche !" onClick={launchApi} />
-        </form>
+        </div>
     )
 }
 
