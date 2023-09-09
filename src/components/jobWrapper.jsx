6f551316609122f9a0b9514ab9b3ee2214ed6ed5
Efrasius <33842173+Efrasius@users.noreply.github.com>
2023-09-09 18:00:29 +0200

    vérif variable jobwrapper
    Ajout d'une vérification de validité de jobs avant de map dessus

--- src/components/jobWrapper.jsx
+++ src/components/jobWrapper.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
-export default function JobType(props) {
-    const { jobs } = props;
-    const { iconPath } = props;
+export default function JobWrapper(props) {
+    const { jobs, iconPath } = props;
+
+
 
     return (
         <div className="jobWrapper">
-            {jobs.map((val, key) => {
+            {jobs.length?jobs.map((val, key) => {
                 return (<div className="job" key={key}>
                     <img className="jobIcon" src={require("../images/" + iconPath + val.jobName + ".png")} alt={val.jobName} />
                     <p className="lvl">{val.level}</p>
                  </div>)
-            })}
+            }):''}
         </div>
     )
 }
